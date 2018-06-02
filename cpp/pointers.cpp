3dd6cff778e8b59c0d15e4d069626eddf75b6c0d
Tushar Semwal <semwaltushar@gmail.com>
2018-06-02 09:07:23 +0530

    Create pointers.cpp

---
+++ cpp/pointers.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int c = *a;
+    *a = *a + *b;
+    *b = c - *b;
+    if(*b<0) *b = *b * -1;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
